45ac5aa886bcd3f909e2b86606d83e732e0c3dc2
GustavoSilvera <gustavo.r.silvera@gmail.com>
2021-04-26 00:15:39 -0700

    added high quality mode

--- CreateMovie.sh
+++ CreateMovie.sh
@@ -13,9 +13,10 @@ rm Out/Movie.mp4 || true
 
 echo -e "\nRendering Movie..."
 # omit all messages (non-verbose), all ppm files in Output directory
+# NOTE: remove "-qscale 0" for a much smaller file (~20x smaller) at a cost of quality
 ffmpeg -hide_banner -loglevel error \
        -pattern_type glob -i 'Out/*.ppm' \
-       -c:v mpeg4 -pix_fmt yuv420p Out/Movie.mp4 
+       -c:v mpeg4 -pix_fmt yuv420p -qscale 0 Out/Movie.mp4 
 echo -e "...Done!"
 # clean directory
 # for f in Out/*.png
