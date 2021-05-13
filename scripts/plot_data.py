e58b3c0203e6afeefc7e737095c93e4c145014dd
GustavoSilvera <gustavo.r.silvera@gmail.com>
2021-05-13 09:49:37 -0700

    adding plotter for arbitrary data

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -2,104 +2,66 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.ticker import StrMethodFormatter
+
 mpl.use('Agg')
 
 # output directory
 results = "py_out/"
 
 
-class data():
-    def __init__(self, y_vals: list, title: str, machine: str):
+class plot():
+    def __init__(self, x_vals, y_vals, title, x_label, y_label):
+        self.x_vals = np.array(x_vals)
         self.y_vals = np.array(y_vals)
-        self.y_speedup = self.y_vals[:-1] / self.y_vals[1:]
         self.title = title
-        self.machine = machine
+        self.x_label = x_label
+        self.y_label = y_label
 
+        def plot_graph():
+            # create a figure that is 6in x 6in
+            fig = plt.figure(figsize=(6, 6))
 
-def plot_graph(data):
-    # create a figure that is 6in x 6in
-    fig = plt.figure(figsize=(6, 6))
+            # the axis limits and grid lines
+            plt.grid(True)
+            plt.xlim(0, self.x_vals[-1])
 
-    # the threads are all integers
-    x_vals = [1, 2, 4, 8, 12, 16, 24, 32]  # num procs
+            # label your graph, axes, and ticks on each axis
+            plt.xlabel(self.x_label, fontsize=16)
+            plt.ylabel(self.y_label, fontsize=16)
+            plt.xticks(self.x_vals)
+            plt.yticks()
+            plt.tick_params(labelsize=15)
+            plt.title(self.title, fontsize=18)
 
-    # the axis limits and grid lines
-    plt.grid(True)
-    plt.xlim(0, x_vals[-1] + 5)
+            # plot the data values, lines and points
+            plt.plot(self.x_vals, self.y_vals,
+                     color='r', linewidth=1)  # red line
+            plt.plot(self.x_vals, self.y_vals, 'bo')  # blue circle
 
-    # label your graph, axes, and ticks on each axis
-    plt.xlabel('Number of Processors', fontsize=16)
-    plt.ylabel('Time', fontsize=16)
-    plt.xticks(x_vals)
-    plt.yticks()
-    plt.tick_params(labelsize=15)
-    plt.title('Timing: ' + data.title + ' on ' + data.machine, fontsize=18)
+            # plot y values at points
+            for i, j in zip(x_vals, self.y_vals):
+                plt.annotate(str(j) + 's', xy=(i + 0.5, j), fontsize=13)
 
-    # plot the data values, lines and points
-    plt.plot(x_vals, data.y_vals, color='r', linewidth=1)  # red line
-    plt.plot(x_vals, data.y_vals, 'bo')  # blue circle
-
-    # plot y values at points
-    for i, j in zip(x_vals, data.y_vals):
-        plt.annotate(str(j) + 's', xy=(i + 0.5, j), fontsize=13)
-
-    # complete the layout, save figure, and show the figure for you to see
-    plt.tight_layout()
-    if (not os.path.exists(os.path.join(os.getcwd(), results))):
-        os.makedirs(results)
-    fig.savefig(os.path.join(results, "Time_" + data.title.replace(" ", "") +
-                             "_" + data.machine + '.png'))
-    # close figure
-    plt.clf()
-
-    """PLOT SPEEDUP"""
-
-    # create a figure that is 6in x 6in
-    fig = plt.figure(figsize=(6, 6))
-
-    # the threads are all integers
-    x_vals = np.log2(x_vals[1:])  # num procs
-
-    # the axis limits and grid lines
-    plt.grid(True)
-    plt.ylim(0, 2.5)  # speedup range should be in [0, 2]
-    plt.xlim(0, x_vals[-1] + 0.5)  # speedup range should be in [0, 2]
-
-    # label your graph, axes, and ticks on each axis
-    plt.xlabel('Log2(Number of Processors)', fontsize=16)
-    plt.ylabel('Speedup', fontsize=16)
-    plt.xticks(x_vals)
-    plt.yticks()
-    plt.tick_params(labelsize=15)
-    plt.title('Speedup: ' + data.title + ' on ' + data.machine, fontsize=18)
-
-    # plot the data values, lines and points
-    plt.plot(x_vals, data.y_speedup, color='r', linewidth=1)  # red line
-    plt.plot(x_vals, data.y_speedup, 'bo')  # blue circle
-
-    # plot y values at points
-    for i, j in zip(x_vals, data.y_speedup):
-        plt.annotate('%.3fx' % j, xy=(i, j + 0.1), fontsize=13)
-     # complete the layout, save figure, and show the figure for you to see
-    plt.tight_layout()
-
-    if (not os.path.exists(os.path.join(os.getcwd(), results))):
-        os.makedirs(results)
-    fig.savefig(os.path.join(results, "Speedup_" + data.title.replace(" ", "") +
-                             "_" + data.machine + '.png'))
+            # complete the layout, save figure, and show the figure for you to see
+            plt.tight_layout()
+            if (not os.path.exists(os.path.join(os.getcwd(), results))):
+                os.makedirs(results)
+            fig.savefig(os.path.join(results,
+                                     self.title.replace(" ", "") + '.png'))
+            # close figure
+            plt.clear()
+            plt.clf()
 
 
 if __name__ == '__main__':
     # laptop data
-    v0 = data([24.587, 13.073, 7.481, 4.436, 3.383, 3.554, 3.283, 3.105],
-              "Parallel Boids", "Global")
-    v1 = data([21.525, 12.914, 7.369, 4.864, 3.842, 3.477, 4.367, 3.120],
-              "Parallel Flocks", "Global")
-    v2 = data([15.267, 9.063, 5.405, 3.326, 2.869, 2.699, 3.683, 3.514],
-              "Parallel Boids", "Local")
-    v3 = data([15.232, 9.573, 5.864, 3.570, 2.796, 2.538, 2.718, 2.308],
-              "Parallel Flocks", "Local")
+    v0 = plot(x_vals=np.arange(100),
+              y_vals=[],
+              title="Tick Timings",
+              x_label="Tick Index",
+              y_label="Time (s)")
 
-    views = [v0, v1, v2, v3]
+    views = [v0]
     for v in views:
-        plot_graph(v)
+        v.plot()
---
+++ scripts/plot_perf_speedup.py
@@ -0,0 +1,109 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.ticker import StrMethodFormatter
+
+mpl.use('Agg')
+
+# output directory
+results = "py_out/"
+
+
+class data():
+    def __init__(self, y_vals: list, title: str, machine: str):
+        self.y_vals = np.array(y_vals)
+        self.y_speedup = self.y_vals[:-1] / self.y_vals[1:]
+        self.title = title
+        self.machine = machine
+
+
+def plot_graph(data):
+    # create a figure that is 6in x 6in
+    fig = plt.figure(figsize=(6, 6))
+
+    # the threads are all integers
+    x_vals = [1, 2, 4, 8, 12, 16, 24, 32]  # num procs
+
+    # the axis limits and grid lines
+    plt.grid(True)
+    plt.xlim(0, x_vals[-1] + 5)
+
+    # label your graph, axes, and ticks on each axis
+    plt.xlabel('Number of Processors', fontsize=16)
+    plt.ylabel('Time', fontsize=16)
+    plt.xticks(x_vals)
+    plt.yticks()
+    plt.tick_params(labelsize=15)
+    plt.title('Timing: ' + data.title + ' on ' + data.machine, fontsize=18)
+
+    # plot the data values, lines and points
+    plt.plot(x_vals, data.y_vals, color='r', linewidth=1)  # red line
+    plt.plot(x_vals, data.y_vals, 'bo')  # blue circle
+
+    # plot y values at points
+    for i, j in zip(x_vals, data.y_vals):
+        plt.annotate(str(j) + 's', xy=(i + 0.5, j), fontsize=13)
+
+    # complete the layout, save figure, and show the figure for you to see
+    plt.tight_layout()
+    if (not os.path.exists(os.path.join(os.getcwd(), results))):
+        os.makedirs(results)
+    fig.savefig(os.path.join(results, "Time_" + data.title.replace(" ", "") +
+                             "_" + data.machine + '.png'))
+    # close figure
+    plt.clf()
+
+    """PLOT SPEEDUP"""
+
+    # create a figure that is 6in x 6in
+    fig = plt.figure(figsize=(6, 6))
+
+    # the threads are all integers
+    x_vals = np.log2(x_vals[1:])  # num procs
+
+    # the axis limits and grid lines
+    plt.grid(True)
+    plt.ylim(0, 2.5)  # speedup range should be in [0, 2]
+    plt.xlim(0, x_vals[-1] + 0.5)  # speedup range should be in [0, 2]
+
+    # label your graph, axes, and ticks on each axis
+    plt.xlabel('Log2(Number of Processors)', fontsize=16)
+    plt.ylabel('Speedup', fontsize=16)
+    plt.xticks(x_vals)
+    plt.yticks()
+    plt.tick_params(labelsize=15)
+    plt.title('Speedup: ' + data.title + ' on ' + data.machine, fontsize=18)
+
+    # plot the data values, lines and points
+    plt.plot(x_vals, data.y_speedup, color='r', linewidth=1)  # red line
+    plt.plot(x_vals, data.y_speedup, 'bo')  # blue circle
+    # 1 decimal on x
+    plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}'))
+
+    # plot y values at points
+    for i, j in zip(x_vals, data.y_speedup):
+        plt.annotate('%.3fx' % j, xy=(i, j + 0.1), fontsize=13)
+     # complete the layout, save figure, and show the figure for you to see
+    plt.tight_layout()
+
+    if (not os.path.exists(os.path.join(os.getcwd(), results))):
+        os.makedirs(results)
+    fig.savefig(os.path.join(results, "Speedup_" + data.title.replace(" ", "") +
+                             "_" + data.machine + '.png'))
+
+
+if __name__ == '__main__':
+    # laptop data
+    v0 = data([21.991, 13.6352, 7.1271, 6.291, 3.762, 3.743, 4.236, 3.231],
+              "10000 Parallel Boids", "Global")
+    v2 = data([12.97, 7.758, 4.105, 3.028, 2.541, 2.489, 3.525, 3.798],
+              "10000 Parallel Boids", "Local")
+    v1 = data([18.442, 12.102, 6.29, 5.224, 4.267, 4.097, 5.233, 4.289],
+              "10000 Parallel Flocks", "Global")
+    v3 = data([13.325, 7.791, 4.177, 3.239, 2.411, 2.388, 3.261, 2.733],
+              "10000 Parallel Flocks", "Local")
+
+    views = [v0, v1, v2, v3]
+    for v in views:
+        plot_graph(v)
